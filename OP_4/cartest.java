211594a20ee72aef28fffddcd72135b31eac1af0
SergeyOleynikov <91291805+SergeyOleynikov@users.noreply.github.com>
2022-06-28 23:15:23 +0300

    Add files via upload

---
+++ OP_4/cartest.java
@@ -0,0 +1,11 @@
+public class cartest {
+    public static void main(String[] args) {
+        lists = new lists();
+        lists.Add(new Sport("mercedes", 2020 , 20000000 , "AMG GT","Germany", "2021", "Олейников Сергей Александрович", "4,5 c.", "3.0 л.", "435 л.с"));
+        lists.Add(new spec("КАМАЗ", 2020, 7100000, "КамАЗ-54901", "Россия", "2020", "Иванов Павел Андреевич", "грузовой", "19 700 кг","длина 6250 мм, ширина 3030 мм, высота 1300мм"));
+        lists.Add(new UsedCar("Toyota", 2021, 3600000, "Camry 3.5", "Япония", "2021", "Елена Головач Алексеевна", "отличная", "Смрнов Александр Иванович", 131000));
+
+        System.out.println(carLists);
+    }
+
+}
